fa4c5680da77c2a26035ef7369ab2ae957d5bc37
dscarrig <dcarrig1@gmail.com>
2021-03-20 14:48:51 -0500

    Added image for missing pictures

--- tvmaze.js
+++ tvmaze.js
@@ -18,17 +18,17 @@
       }
  */
 async function searchShows(query) {
-  // TODO: Make an ajax request to the searchShows api.  Remove
-  // hard coded data.
 
-  return [
-    {
-      id: 1767,
-      name: "The Bletchley Circle",
-      summary: "<p><b>The Bletchley Circle</b> follows the journey of four ordinary women with extraordinary skills that helped to end World War II.</p><p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their normal lives, modestly setting aside the part they played in producing crucial intelligence, which helped the Allies to victory and shortened the war. When Susan discovers a hidden code behind an unsolved murder she is met by skepticism from the police. She quickly realises she can only begin to crack the murders and bring the culprit to justice with her former friends.</p>",
-      image: "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-    }
-  ]
+  const result = await axios.get(`http://api.tvmaze.com/search/shows?q=${query}`);
+  const showArray = []
+
+  for(let i = 0; i < result.data.length; i++){
+    showArray[i] = result.data[i].show;
+  }
+
+  console.log(showArray);
+
+  return showArray;
 }
 
 
@@ -38,23 +38,42 @@ async function searchShows(query) {
  */
 
 function populateShows(shows) {
+  let image;
   const $showsList = $("#shows-list");
   $showsList.empty();
 
   for (let show of shows) {
+    if(show.image){
+      image = show.image.medium;
+    }
+    else{
+      image = "600px-No_image_available.svg.png";
+    }
+
     let $item = $(
       `<div class="col-md-6 col-lg-3 Show" data-show-id="${show.id}">
-         <div class="card" data-show-id="${show.id}">
+         <div class="card" data-show-id="${show.id}" id="${show.id}">
            <div class="card-body">
              <h5 class="card-title">${show.name}</h5>
              <p class="card-text">${show.summary}</p>
+             <img class="card-img-top" src=${image}>
+              <br>
+             <button class="btn btn-primary" id="ep-button">Episodes</button>
            </div>
+
+           <section style="display: none" id="episodes-area">
+            <h3>Episodes</h3> 
+            <ul id="episodes-list">
+            </ul>
+          </section>  
+           
          </div>
        </div>
       `);
 
     $showsList.append($item);
   }
+
 }
 
 
@@ -76,15 +95,44 @@ $("#search-form").on("submit", async function handleSearch (evt) {
   populateShows(shows);
 });
 
+//Adds listener to episode button
+document.querySelector("body").addEventListener("click", function(e){
+  if(e.target.id === "ep-button"){
+    const id = e.target.parentElement.parentElement.getAttribute("data-show-id");
+    populateEpisodes(id);
+  }
+})
 
-/** Given a show ID, return list of episodes:
- *      { id, name, season, number }
- */
-
+//Returns array of episode data
 async function getEpisodes(id) {
-  // TODO: get episodes from tvmaze
-  //       you can get this by making GET request to
-  //       http://api.tvmaze.com/shows/SHOW-ID-HERE/episodes
+  const episodes = await axios.get(`http://api.tvmaze.com/shows/${id}/episodes`);
 
-  // TODO: return array-of-episode-info, as described in docstring above
+  return episodes.data;
+}
+
+//Populate episodes in the episode list
+async function populateEpisodes(id) {
+
+  const $thisShow = $(`#${id}`);
+  const episodes = await getEpisodes(id);
+
+  if($thisShow.find("#episodes-area").attr("style") === "display: none"){
+
+    for (let episode of episodes) {
+      let $item = $(
+        `<div class="col-12"">
+             <div class="card-body">
+               <h5 class="card-title">${episode.name}</h5>
+               <p class="card-text">${episode.airdate}</p>
+               <p class="card-text">${episode.summary}</p>
+             </div>
+           </div>
+         </div>
+        `);
+  
+      $thisShow.find("#episodes-list").append($item);
+    }
+    $thisShow.find("#episodes-area").attr("style", "display: block");
+  }
+
 }
